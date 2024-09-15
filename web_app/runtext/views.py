81316880caf8fe1f64da73f10fbf434586ab64c6
Bogdan Yakupov <zimmermann2hk@gmai.com>
2024-09-15 20:24:21 +0300

    added video view

--- web_app/runtext/views.py
+++ web_app/runtext/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import FileResponse
+from utils.utils import create_video
 
-# Create your views here.
+def index(request):
+    message = str(request.GET.get("message", ""))
+
+    create_video(message)
+    video = open("runtext/utils/video/video.mp4", "+rb")
+
+    return FileResponse(video, content_type="video/mp4v")
